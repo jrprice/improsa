5945c1990f3a928a608590e427d7106b13f9375b
James Price <j.price@bristol.ac.uk>
2014-02-04 16:01:16 +0000

    Added source image and ImageView widget.

--- src/com/jprice/improsa/ImProSA.java
+++ src/com/jprice/improsa/ImProSA.java
@@ -1,15 +1,29 @@
 package com.jprice.improsa;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ImProSA extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  Bitmap original;
+  ImageView imageResult;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    // Load source image
+    original = BitmapFactory.decodeResource(
+      this.getResources(),
+      R.drawable.baboon);
+
+    imageResult = (ImageView)findViewById(R.id.imageResult);
+    imageResult.setImageBitmap(original);
+  }
 }
